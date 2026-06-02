601396b37d6973143c7c9b2d54691cda3ca9ef75
Sarai <saraimontes170@gmail.com>
2026-06-02 00:02:53 -0600

    proyecto1

---
+++ clase/act1-react/src/App.jsx
@@ -0,0 +1,13 @@
+import Tarjeta from './Tarjeta';
+
+function App() {
+  return (
+    <div>
+      <h1>Tarjeta de Presentación</h1>
+      {/* Renderizamos el componente Tarjeta */}
+      <Tarjeta />
+    </div>
+  );
+}
+
+export default App;
